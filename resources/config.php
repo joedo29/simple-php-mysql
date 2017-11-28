421509c1f0771d555ac47e632a9ac2ed397be0dd
Joe Do <joedo@Joes-MacBook-Pro.local>
2017-11-27 22:43:57 -0800

    config for solution2

--- resources/config.php
+++ resources/config.php
@@ -4,17 +4,17 @@ ob_start();
 
 defined("DS") ? null : define("DS",DIRECTORY_SEPARATOR);
 defined("FRONT") ? null : define("FRONT",__DIR__.DS."front");
-defined("DB_HOST") ? null : define("DB_HOST","localhost:3306");
+defined("DB_HOST") ? null : define("DB_HOST","mariadb:3306");
 defined("DB_USER") ? null : define("DB_USER","root");
-defined("DB_PASS") ? null : define ("DB_PASS", "whistler");
-defined("DB_NAME") ? null : define ("DB_NAME", "BookShare_test");
+defined("DB_PASS") ? null : define ("DB_PASS", "root");
+defined("DB_NAME") ? null : define ("DB_NAME", "joe");
 
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS);
 
 if(!$connection){
     die("no connection");
 }
-require_once("functions.php");// creates link to functions so we can excess them 
+require_once("functions.php");// creates link to functions so we can excess them
 
 
 
