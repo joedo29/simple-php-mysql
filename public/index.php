532dc0d0e7f33c7434f2e00dc712449817719a3d
Joe Do <joedo@Joes-MacBook-Pro.local>
2017-11-27 22:13:48 -0800

    Final Submission

--- public/index.php
+++ public/index.php
@@ -1,19 +1,87 @@
 <?php
-$app = require __DIR__ . '/../bootstrap/app.php';
-$data = array_merge($_POST, $_GET);
-$controller = new \App\Controllers\IndexController();
-$action = $data['action'];
-//$users = $controller->searchUsers();
-if (!$data['action']) {
-    include 'users.php';
-}
-//$data = ['title' => 'huhed', 'action' => 'search']
-$results = $controller->handle($data);
-if ($action === 'signup') {
-    include 'list_users.php';
-    return;
-}
-if (in_array($action, ['addBook', 'search', 'delete']))
-    include 'list_books.php';
+ require_once("../resources/config.php");
+include(FRONT.DS."header.php");
 
 ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+              <h4 class="text-center bg-success"><?php display_message(); ?></h4>
+
+
+            <div class="col-md-9">
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-md-offset-3">
+                            <h1>Search for a book</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 col-md-offset-3">
+                            <form action="booksearch.php" class="search-form">
+                               
+                                <div class="form-group has-feedback">
+                                    <label for="search" class="sr-only">Search</label>
+                                    <input type="text" class="form-control" name="search" id="search" placeholder="search">
+                                    <span class="glyphicon glyphicon-search form-control-feedback"></span>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                </div>
+
+                <div class="row">
+
+                    <?php display_books(); ?>
+
+         
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <div class="container">
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2017</p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <script>
+    document.onkeydown=function(evt){
+        var keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
+        if(keyCode == 13)
+        {
+            //your function call here
+            document.test.submit();
+        }
+    }
+</script>
+
+</body>
+
+</html>
